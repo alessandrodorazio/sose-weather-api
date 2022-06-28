90509e8e4b7567fcea8039f9c355b27a44053161
Alessandro D'Orazio <alessandrodorazio@Alessandros-MacBook-Pro.local>
2022-06-28 18:38:05 +0200

    refactoring

--- src/main/java/it/univaq/aggm/CityRepository.java
+++ src/main/java/it/univaq/aggm/CityRepository.java
@@ -17,11 +17,21 @@ public class CityRepository implements CityRepositoryInterface {
 	@GET
     @Path("getWeather/{cityName}")
     public City getCourse(@PathParam("cityName") String cityName) throws IOException, JSONException {
+		
+		JSONObject fetchedData = getData(cityName);
+		JSONObject current = (JSONObject) fetchedData.get("current");
+		City c = new City();
+		c.setName(fetchedData.getJSONObject("location").getString("name"));
+		c.setTemperature(Float.parseFloat(current.get("temp_c").toString()));
+		c.setWeather(((JSONObject)current.get("condition")).get("text").toString()) ;
+        return c;
+    }
+	
+	private JSONObject getData(String cityName) throws IOException, JSONException {
 		OkHttpClient client = new OkHttpClient();
-
+		String url ="https://weatherapi-com.p.rapidapi.com/current.json?q=" + cityName;
 		Request request = new Request.Builder()
-			.url("https://weatherapi-com.p.rapidapi.com/current.json?q=" + cityName)
-			.get()
+			.url(url).get()
 			.addHeader("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
 			.addHeader("X-RapidAPI-Key", "ITzvQqsKNgmshs8XqVDlBbKhtxe0p1cANiTjsn8iql5X6IiJej")
 			.build();
@@ -29,12 +39,6 @@ public class CityRepository implements CityRepositoryInterface {
 		Response response = client.newCall(request).execute();
 		String jsonData = response.body().string();
 	    JSONObject Jobject = new JSONObject(jsonData);
-	    JSONObject current = (JSONObject) Jobject.get("current");
-		
-		City c = new City();
-		c.setName(cityName);
-		c.setTemperature(Float.parseFloat(current.get("temp_c").toString()));
-		c.setWeather(((JSONObject)current.get("condition")).get("text").toString()) ;
-        return c;
-    }
+	    return Jobject;
+	}
 }
